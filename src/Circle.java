af97bc8e45805d10052e44d4cf42c84c93ecf0d5
khaitrinh1207 <khaitrinh1207@gmail.com>
2020-12-20 15:09:27 +0700

    add file comparable

---
+++ src/Circle.java
@@ -0,0 +1,36 @@
+public class Circle {
+    private double radius;
+    private String color;
+    private boolean filled;
+
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        this.radius = radius;
+        this.color = color;
+        this.filled = true;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    public double getPerimeter() {
+        return 2 * radius * Math.PI;
+    }
+
+}
